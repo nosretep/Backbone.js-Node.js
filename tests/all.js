c965ac2865a82a0e1cfb0bc75183f2c2b81f05a6
Mark Peterson <playerspeak@gmail.com>
2014-09-13 00:35:43 -0400

    Removed unnecessary paths

--- tests/all.js
+++ tests/all.js
@@ -12,12 +12,6 @@ requirejs.config({
         'models': 'models',
         'collections' : 'collections',
         'views' : 'views',
-        
-        'jquery' : 'libs/jquery',
-        'underscore' : 'libs/underscore',
-        'backbone' : 'libs/backbone',
-        'bootstrap' : 'libs/bootstrap',
-
         'text': 'libs/text',
         'json': 'libs/json',
         'tests': '../../tests'
