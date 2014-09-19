64578bc5834ea173081e3fb72d356e7f87b11b0f
Mark Peterson <playerspeak@gmail.com>
2014-09-18 20:24:39 -0400

    process.env.port

--- server.js
+++ server.js
@@ -102,7 +102,7 @@ function(
     }
 
     // Configure server ...
-    server.set('port', 8888);
+    server.set('port', (process.env.PORT || 5000))
     server.engine('.html', require('ejs').__express);
     server.set('views', __dirname + '/');
 
