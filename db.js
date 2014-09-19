570b5316cbcc9c085ec08cabdbdf392ba2eb5b5c
Mark Peterson <playerspeak@gmail.com>
2014-09-19 02:18:20 -0400

    var db

--- db.js
+++ db.js
@@ -1,112 +1,107 @@
-define(['mongodb', 'jquery', 'underscore'],
-function(mongo, $, _) {    
+define([
+	'mongodb', 
+	'jquery', 
+	'underscore'],
 
-	var Server = mongo.Server,
-    Db = mongo.Db,
-    BSON = mongo.BSONPure;
-	
-	
-	var server = new Server('127.0.0.1', 27017, {}),
-	db = new Db('thingdb', server, {safe:true});
+	function(mongo, $, _) {    
 
-	db.open(function(err, db) {
-	    if(!err) {
-	        console.log("Connected to 'thingdb' database");
-	        db.collection('things', {strict:true}, function(err, collection) {
-	            if (err) {
-	                
-	            }
-	        });
-	    } else {
-	    	console.log(err.message);
-	    }
-	});
+		var Server = mongo.Server,
+	    	Db = mongo.Db,
+	    	BSON = mongo.BSONPure;
+		
+		var server = new Server('127.0.0.1', 27017, {}),
+		var db = new Db('thingdb', server, {safe:true});
 	
-	function _idToId(item) {
-    	item.id = item._id;
-    	delete item._id;
-    	return item;
-	}
+		db.open(function(err, db) {
+			if (!err) {
+				console.log("Connected to 'thingdb' database");
+				db.collection('things', {strict : true}, function(err, collection) {
+					
+				});
+			} else {
+				console.log(err.message);
+			}
+		});
+
+		function _idToId(item) {
+			item.id = item._id;
+			delete item._id;
+			return item;
+		}
 	
-	function idTo_Id(item) {
-    	item._id = item.id;
-    	delete item.id;
-    	return item;
-	}
-	
-	return {
-		Things: {
-			findById: function(id) {
-			    var deferred = $.Deferred();
-			    db.collection('things', function(err, collection) {
-			        collection.findOne({'_id':new BSON.ObjectID(id)}, function(err, item) {
-			        	_idToId(item);
-			        	deferred.resolve(item);
-			        });
-			    });
-			    return deferred.promise();
-			}
-			,
-			findAll: function() {
-				var deferred = $.Deferred();
-			    db.collection('things', function(err, collection) {
-			        collection.find().toArray(function(err, items) {
-			        	_.each(items, function(item) {
-			        		_idToId(item);
-			        	});
-			        	deferred.resolve(items);
-			        });
-			    });
-			    return deferred.promise();
-			}
-			,
-			add: function(data) {
-				var deferred = $.Deferred();
-			    db.collection('things', function(err, collection) {
-			        collection.insert(data, {safe:true}, function(err, results) {
-			            if (err) {
-			            	deferred.reject(err);
-			            } else {
-			            	var item = results[0];
-			            	_idToId(item);
-			            	deferred.resolve(item);
-			            }
-			        });
-			    });
-			    return deferred.promise();
-			}
-			,
-			update: function(data) {
-				var deferred = $.Deferred();
-				var id = data.id;
-				delete data.id;
-			    db.collection('things', function(err, collection) {
-			        collection.update({'_id':new BSON.ObjectID(id)}, data, {safe:true}, function(err, results) {
-			            if (err) {
-			            	deferred.reject(err);
-			            } else {
-			            	data.id = id;
-			            	deferred.resolve(data);
-			            }
-			        });
-			    });
-			    return deferred.promise();
-			}
-//			,
-//			delete: function(data) {
-//			    var id = data.id;
-//			    db.collection('things', function(err, collection) {
-//			        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
-//			            if (err) {
-//			                res.send({'error':'An error has occurred - ' + err});
-//			            } else {
-//			                console.log('' + result + ' document(s) deleted');
-//			                res.send(req.body);
-//			            }
-//			        });
-//			    });
-//			}
-		}		
-	}
+		return {
+			Things: {
+				findById : function(id) {
+					var deferred = $.Deferred();
+					db.collection('things', function(err, collection) {
+						collection.findOne({'_id' : new BSON.ObjectID(id) }, function(err, item) {
+							_idToId(item);
+							deferred.resolve(item);
+						});
+					});
+					return deferred.promise();
+				}
+				,
+				findAll : function() {
+					var deferred = $.Deferred();
+					db.collection('things', function(err, collection) {
+						collection.find().toArray(function(err, items) {
+							_.each(items, function(item) {
+								_idToId(item);
+							});
+							deferred.resolve(items);
+						});
+					});
+					return deferred.promise();
+				}
+				,
+				add : function(data) {
+					var deferred = $.Deferred();
+					db.collection('things', function(err, collection) {
+						collection.insert(data, { safe : true }, function(err, results) {
+							if (err) {
+								deferred.reject(err);
+							} else {
+								var item = results[0];
+								_idToId(item);
+								deferred.resolve(item);
+							}
+						});
+					});
+					return deferred.promise();
+				}
+				,
+				update : function(data) {
+					var deferred = $.Deferred();
+					var id = data.id;
+					delete data.id;
+					db.collection('things', function(err, collection) {
+						collection.update({'_id' : new BSON.ObjectID(id)}, data, {safe : true}, function(err, results) {
+							if (err) {
+								deferred.reject(err);
+							} else {
+								data.id = id;
+								deferred.resolve(data);
+							}
+						});
+					});
+					return deferred.promise();
+				}
+	// ,
+	// delete: function(data) {
+	// var id = data.id;
+	//			    db.collection('things', function(err, collection) {
+	//			        collection.remove({'_id':new BSON.ObjectID(id)}, {safe:true}, function(err, result) {
+	//			            if (err) {
+	//			                res.send({'error':'An error has occurred - ' + err});
+	//			            } else {
+	//			                console.log('' + result + ' document(s) deleted');
+	//			                res.send(req.body);
+	//			            }
+	//			        });
+	//			    });
+	//			}
+			}		
+		}
 	
 });
