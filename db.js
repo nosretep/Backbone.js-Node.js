69852310329299d3cda56fb8abec83bfebce7819
Mark Peterson <playerspeak@gmail.com>
2014-09-19 02:19:37 -0400

    var db

--- db.js
+++ db.js
@@ -10,7 +10,7 @@ define([
 	    	BSON = mongo.BSONPure;
 		
 		var server = new Server('127.0.0.1', 27017, {}),
-		var db = new Db('thingdb', server, {safe:true});
+			db = new Db('thingdb', server, {safe:true});
 	
 		db.open(function(err, db) {
 			if (!err) {
