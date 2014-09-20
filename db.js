6c15991b72abbb15c1141961862984a5cbb481bf
Mark Peterson <playerspeak@gmail.com>
2014-09-19 22:51:55 -0400

    Dynamic db configs

--- db.js
+++ db.js
@@ -9,20 +9,37 @@ define([
 		var Server = mongo.Server,
 	    	Db = mongo.Db,
 	    	BSON = mongo.BSONPure;
-		
-		var server = new Server('127.0.0.1', 27017, {}),
+
+		var db_domain = process.env.DB_DOMAIN || '127.0.0.1';
+		var db_port = process.env.DB_PORT || '27017';
+		var db_username = process.env.DB_USERNAME || '';
+		var db_password = process.env.DB_PASSWORD || '';
+				
+		var server = new Server(db_domain, db_port, {}),
 			db = new Db('thingdb', server, {safe:true});
-	
+		
 		db.open(function(err, db) {
 			if (!err) {
-				console.log("Connected to 'thingdb' database");
-				db.collection('things', {strict : true}, function(err, collection) {
-					
-				});
+				function onOpen(db) {
+					console.log("Connected to 'thingdb' database");
+					db.collection('things', {strict : true}, function(err, collection) {});						
+				}
+				
+				if (db_username && db_password) {
+					db.authenticate(db_username, db_password, function(err, res) {
+						onOpen(db);
+					});					
+				} else {
+					onOpen(db);
+				}
+				
 			} else {
 				console.log(err.message);
 			}
+
 		});
+	
+
 
 		function fixId(item) {
 			item.id = item._id;
